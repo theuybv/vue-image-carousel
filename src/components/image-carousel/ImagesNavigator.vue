b82f0da872ad57911ec10695cfe3808084ef764b
Theuy Limpanont <info@theuy.nl>
2022-04-24 16:58:41 +0200

    style: 💄 Navigators
    thumbs rounded full

--- src/components/image-carousel/ImagesNavigator.vue
+++ src/components/image-carousel/ImagesNavigator.vue
@@ -26,13 +26,13 @@ const onPrevClick = () => {
 <template>
   <section class="absolute w-full h-full top-0 pointer-events-none">
     <nav class="relative flex h-full items-center">
-      <button :disabled="!showPrev" @click="onPrevClick"  class="disabled:opacity-30 absolute bg-white left-0 pointer-events-auto">
+      <button :disabled="!showPrev" @click="onPrevClick"  class="disabled:opacity-30 absolute bg-white left-2 rounded-full pointer-events-auto">
         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"
              stroke-width="2">
           <path stroke-linecap="round" stroke-linejoin="round" d="M15 19l-7-7 7-7"/>
         </svg>
       </button>
-    <button :disabled="!showNext" @click="onNextClick" class="disabled:opacity-30 absolute bg-white right-0 pointer-events-auto">
+    <button :disabled="!showNext" @click="onNextClick" class="disabled:opacity-30 absolute bg-white right-2 rounded-full pointer-events-auto">
         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"
              stroke-width="2">
           <path stroke-linecap="round" stroke-linejoin="round" d="M9 5l7 7-7 7"/>
