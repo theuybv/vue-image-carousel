2ac47b23520b1bb6e119c70f74e769a606953066
Theuy Limpanont <info@theuy.nl>
2022-04-26 09:14:26 +0200

    test: 💍 ImageDisplay
    loading icon test

--- src/components/image-carousel/__tests__/ImageDisplay.spec.ts
+++ src/components/image-carousel/__tests__/ImageDisplay.spec.ts
@@ -1,12 +1,13 @@
-import { describe, expect, it, beforeEach, vi } from "vitest";
+import { describe, expect, it, beforeEach, vi, MockedFunction } from "vitest";
 import { ref } from "vue";
 import { mountWithImageCarouselProvider } from "./utils";
 import ImageDisplay from "../ImageDisplay.vue";
 import Loader from "vue-spinner/src/BeatLoader.vue";
+import { computedAsync } from "@vueuse/core";
 
 vi.mock("@vueuse/core", () => {
   return {
-    computedAsync: vi.fn().mockReturnValue(ref("laodedImage")),
+    computedAsync: vi.fn(),
     useClamp: vi.fn().mockReturnValue(ref(0)),
     useElementSize: vi.fn().mockReturnValue({ width: ref(0) }),
     useIntervalFn: vi.fn(),
@@ -14,6 +15,12 @@ vi.mock("@vueuse/core", () => {
 });
 describe("ImageDisplay", () => {
   it("should load image", () => {
+    const computedAsyncMock = computedAsync as MockedFunction<
+      typeof computedAsync
+    >;
+
+    computedAsyncMock.mockReturnValue(ref("loadedimage"));
+
     const wrapper = mountWithImageCarouselProvider(ImageDisplay, {
       images: [
         {
@@ -26,5 +33,26 @@ describe("ImageDisplay", () => {
 
     expect(wrapper.find("figure img").exists()).toBeTruthy();
     expect(wrapper.findComponent(Loader).exists()).not.toBeTruthy();
+  });
+
+  it("should show loading icon when image is loading", () => {
+    const computedAsyncMock = computedAsync as MockedFunction<
+      typeof computedAsync
+    >;
+
+    computedAsyncMock.mockReturnValue(ref(undefined));
+
+    const wrapper = mountWithImageCarouselProvider(ImageDisplay, {
+      images: [
+        {
+          imageSrc: "",
+          alt: "",
+          thumbSrc: "",
+        },
+      ],
+    });
+
+    expect(wrapper.find("figure img").exists()).toBeFalsy();
+    expect(wrapper.findComponent(Loader).exists()).toBeTruthy();
   });
 });
